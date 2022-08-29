4d759e96fb521f1a8b3361454a378505d1514a05
damianrequejo <drequejo@gmail.com>
2022-08-29 13:41:58 -0300

    Incorporo Productos

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,12 +1,43 @@
-import React from 'react'
-import './itemlistcontainer.css';
+import React, { useState, useEffect } from 'react';
+import { products } from '../../mock/products';
 
-const ItemListContainer = ({saludo}) => {
-  
-  return (
-   
-    <p>{saludo}</p>
-  )
-}
+const ItemListContainer = ({ saludo }) => {
+    const [items, setItems] = useState([]);
 
-export default ItemListContainer
+    useEffect(() => {
+        const getProducts = new Promise((res, rej) => {
+            setTimeout(() => {
+                res(products);
+            }, 2000);
+        });
+
+        getProducts
+            .then((data) => {
+                setItems(data);
+            })
+            .catch((error) => {
+                //console.log(error);
+            })
+            .finally(() => {
+                //console.log('Finally');
+            });
+    }, []);
+
+    return (
+        <div>
+            <h2>{saludo}</h2>
+            {items.map((item) => {
+                return (
+                    //la key luego pasa al componente Item --> <Item props={props} key={key}/>
+                    <div key={item.id}>
+                        <p>{item.title}</p>
+                        <h5>${item.price}.-</h5>
+                        <img src={item.img} alt="" />
+                    </div>
+                );
+            })}
+        </div>
+    );
+};
+
+export default ItemListContainer;
---
+++ src/mock/products.js
@@ -0,0 +1,34 @@
+export const products = [
+    {
+        id: 1,
+        title: 'Visa Classic',
+        price: 200,
+        stock: 10,
+        category: 'tarjetas de crédito',
+        img: '../img/tc-visa.png',
+    },
+    {
+        id: 2,
+        title: 'Visa Gold',
+        price: 400,
+        stock: 10,
+        category: 'tarjetas de crédito',
+        img: '../img/tc-visa.png',
+    },
+    {
+        id: 3,
+        title: 'Visa Platinium',
+        price: 600,
+        stock: 10,
+        category: 'tarjetas de crédito',
+        img: '../img/tc-visa.png',
+    },
+    {
+        id: 4,
+        title: 'Visa Débito',
+        price: 0,
+        stock: 10,
+        category: 'tarjetas de débito',
+        img: '../img/tc-visa.png',
+    },
+]
